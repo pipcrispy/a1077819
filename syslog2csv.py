cd2b93ec271d37b8fdc98d5a9f1b5c5d6e850925
David Morris <gm3dmo@github.com>
2021-03-25 10:42:44 +0000

    Adding workflow.

---
+++ syslog2csv.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+__version__ = "0.1.0"
+
+import re
+import argparse
+import logging
+
+
+def main(args):
+    print("hello")
+
+    woo =      7
+    print("""zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz""")
+
+
+# "timestamp": real_datetime.timestamp(),
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="a syslog file")
+    # parser.add_argument("-m", "--mode", action="store_true", default=False)
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+        help="debug(-d, --debug, etc)",
+    )
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__),
+    )
+
+    args = parser.parse_args()
+
+    main(args)
